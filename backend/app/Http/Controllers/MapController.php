71a5eb8a83dcce9b7ff8cdf69114f8dca0afb0ec
Addin Gama Bertaqwa <addin.gama@wgs.co.id>
2015-07-01 15:15:35 +0700

    update

--- backend/app/Http/Controllers/MapController.php
+++ backend/app/Http/Controllers/MapController.php
@@ -7,6 +7,15 @@ use DB;
 
 class MapController extends Controller
 {
+
+    public function home() {
+
+        $result = [
+            'message' => 'Your api server working'
+        ];
+        return response()->json($result);
+    }
+
     public function index(Request $request) {
         $query = 'select * from data';
         if ($request->has('type')) {
@@ -25,7 +34,75 @@ class MapController extends Controller
             }
         }
         $result = DB::select($query);
-        // return (new Response($result, 200))->header("Access-Control-Allow-Origin", "*");
         return response()->json($result);
+    }
+
+    public function getDates(Request $request) {
+        $dates = DB::table('data')
+                    ->select('tanggal')
+                    ->groupBy('tanggal')
+                    ->get();
+
+        return $dates;
+    }
+    public function getPhoneByDate(Request $request, $date) {
+        $dates = DB::table('data')
+                    ->select('MSISDN')
+                    ->where('tanggal', '=', $date)
+                    ->groupBy('MSISDN')
+                    ->get();
+
+        return count($dates);
+    }
+
+    public function getPhones(Request $request) {
+        $phones = DB::table('numbers')
+                    ->select('*')
+                    ->get();
+
+        $result = [];
+        foreach ($phones as $key => $value) {
+            array_push($result, $value->MSISDN);
+        }
+
+        return $result;
+    }
+
+    public function filterNumber(Request $request, $date, $number) {
+        $result[$number] = DB::table('data')
+                    ->select('*')
+                    ->where('tanggal', '=', $date)
+                    ->where('MSISDN', '=', $number)
+                    ->get();
+
+
+        return $result;
+    }
+
+    public function filterRange(Request $request, $date, $numbers, $start) {
+        $phones = DB::table('numbers')
+                    ->select('*')
+                    ->skip($start)
+                    ->take($numbers)
+                    ->get();
+
+        $p = [];
+        foreach ($phones as $key => $value) {
+            array_push($p, $value->MSISDN);
+        }
+
+        $data = DB::table('data')
+                ->whereIn('MSISDN', $p)
+                ->get();
+
+        // prepare result array
+        foreach ($p as $value) {
+            $result[$value] = [];
+        }
+        foreach ($data as $key => $value) {
+            array_push($result[$value->MSISDN], $value);
+        }
+
+        return $result;
     }
 }
