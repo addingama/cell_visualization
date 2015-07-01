283f1560c204dee34f49f1f38f7deb624b8fc338
Addin Gama Bertaqwa <addin.gama@wgs.co.id>
2015-07-01 12:59:18 +0700

    update php limit

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -10,5 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+ini_set('memory_limit','1024M');
 $app->get('/', 'App\Http\Controllers\MapController@index');
+$app->get('/tanggal', 'App\Http\Controllers\MapController@getDates');
+$app->get('/tanggal/{date}/phones', 'App\Http\Controllers\MapController@getPhoneByDate');
+$app->get('/phones', 'App\Http\Controllers\MapController@getPhones');
+
